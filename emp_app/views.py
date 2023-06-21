6384cc4bee57840db8bc5c0440e953b53a6676bc
Shivam8905 <shivamdubey890513@gmail.com>
2023-06-21 12:42:56 +0530

    updated

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.db.models import Q
 from django.shortcuts import render, HttpResponse
 from .models import Employee, Role, Department
 
@@ -42,10 +43,45 @@ def add_emp(request):
 
 
 
-def remove_emp(request):
-    return render(request, 'remove_emp.html')
+def remove_emp(request, emp_id = 0):
+    if emp_id:
+        try:
+            emp_to_be_removed = Employee.objects.get(id = emp_id)
+            emp_to_be_removed.delete()
+            return HttpResponse("Employee Removed Successfully")
+        except:
+            return HttpResponse("Please Enter A valid Emp ID")
+    emps = Employee.objects.all()
+    context = {
+        'emps' : emps
+    }
+
+    return render(request, 'remove_emp.html', context)
 
 
 def filter_emp(request):
-    return render(request, 'filter_emp.html')
+    if request.method == "POST":
+        name = request.POST.get('name')
+        dept = request.POST.get('department')
+        role = request.POST.get('role')
+
+        emps = Employee.objects.all()
+        if name:
+            emps = emps.filter(Q(first_name__icontains=name) | Q(last_name__icontains=name))
+        if dept:
+            emps = emps.filter(dept__name=dept)
+        if role:
+            emps = emps.filter(role__name=role)
+
+        context = {
+            'emps' : emps
+        }
+        return render(request, 'all_emp.html', context)
+
+    elif request.method == 'GET':
+        return render(request, 'filter_emp.html')
+
+    else:
+        return HttpResponse("Error")
+
 
